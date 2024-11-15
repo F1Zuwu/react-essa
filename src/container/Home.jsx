1ef95c225dc487be5cceb0e555db5c2aaf5abb5d
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2024-11-15 13:56:20 +0200

    mhm

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ExpenseItem from "../components/ExpenseItem";
 
 const Home = () => {
+
+    const expenses = [
+        {
+            date: new Date(2024,10,12),
+            title: 'New book',
+            price: 30.99
+        },
+        {
+            date: new Date(2024, 10, 12),
+            title: "New Jeans",
+            price: 99.99
+        }
+    ]
     return(
         <div class="">
-            <ExpenseItem></ExpenseItem>
+            <ExpenseItem data={expenses[0]}></ExpenseItem>
+            <ExpenseItem data={expenses[1]}></ExpenseItem>
         </div>
     )
 }
