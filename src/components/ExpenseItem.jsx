6d06036bbee47bd922ce2040b04812df2e18944c
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2024-11-15 15:04:40 +0200

    Ülesanne 3 - ümbritseva komponendi rakendamine teistes komponentides

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 const ExpenseItem = (props) => {
 
     return(
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.data.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2 class="text-center font-bold">{props.data.title}</h2>
                 <div className="expense-item__price">{props.data.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
